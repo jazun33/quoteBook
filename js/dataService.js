b23f999ad016493e363dd0d7d6534ea35953495f
jazun33 <jasonmarlow@gmail.com>
2015-04-30 20:38:37 -0600

    finished WITH cookies and all!

--- js/dataService.js
+++ js/dataService.js
@@ -1,15 +1,23 @@
 var app = angular.module('quoteBook');
 
-app.service('dataService', function() {
-    var quotes = [
-        { text: 'Life isn\'t about getting and having, it\'s about giving and being.', author: 'Kevin Kruse'},
-        { text: 'Whatever the mind of man can conceive and believe, it can achieve', author: 'Napoleon Hill'},
-        { text: 'Strive not to be a success, but rather to be of value.', author: 'Albert Einstein'},
-        { text: 'Two roads diverged in a wood, and I took the one less traveled by, And that has made all the difference.', author: 'Robert Frost'},
-        { text: 'The most difficult thing is the decision to act, the rest is merely tenacity.', author: 'Amelia Earhart'},
-        { text: 'Life is what happens to you while you\'re busy making other plans.', author: 'John Lennon'},
-        { text: 'What even is a jQuery?', author: 'Tyler S. McGinnis'}
-    ];
+app.service('dataService', function($cookies) {
+
+    var quotes = [];
+
+    if ($cookies.quotes === undefined) {
+        quotes = [
+            { text: 'Life isn\'t about getting and having, it\'s about giving and being.', author: 'Kevin Kruse'},
+            { text: 'Whatever the mind of man can conceive and believe, it can achieve', author: 'Napoleon Hill'},
+            { text: 'Strive not to be a success, but rather to be of value.', author: 'Albert Einstein'},
+            { text: 'Two roads diverged in a wood, and I took the one less traveled by, And that has made all the difference.', author: 'Robert Frost'},
+            { text: 'The most difficult thing is the decision to act, the rest is merely tenacity.', author: 'Amelia Earhart'},
+            { text: 'Life is what happens to you while you\'re busy making other plans.', author: 'John Lennon'},
+            { text: 'What even is a jQuery?', author: 'Tyler S. McGinnis'}
+        ];
+    } else {
+        quotes = JSON.parse($cookies.quotes);
+        console.log(quotes);
+    }
 
     this.getData = function() {
         return quotes;
@@ -17,10 +25,16 @@ app.service('dataService', function() {
 
     this.addData = function(dataObj) {
         quotes.unshift(dataObj);
+        this.saveData();
     };
 
     this.removeData = function(dataObj) {
         quotes.splice(quotes.indexOf(dataObj), 1);
+        this.saveData();
+    };
+
+    this.saveData = function () {
+        $cookies.quotes = JSON.stringify(quotes);
     };
 
 });
--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -1,8 +1,9 @@
 var app = angular.module('quoteBook');
 
 app.controller("mainCtrl", function($scope, dataService){
-    //$scope.lastData = $cookies.getObject('data');
+
     $scope.quotes = dataService.getData();
+
 
     $scope.addData = function(item) {
         dataService.addData(item);
@@ -11,6 +12,4 @@ app.controller("mainCtrl", function($scope, dataService){
     $scope.removeData = function(item) {
         dataService.removeData(item);
     }
-
-    //$cookies.putObject('data', dataService);
 });
